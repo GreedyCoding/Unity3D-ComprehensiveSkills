503c4b14103a543dfc376cf36fb0a338b163e8a4
GreedyCoding <marco_hofer2012@outlook.com>
2018-09-01 02:54:35 +0200

    Finished - Added FPS Counter to the Atomic Nucleus
    Highest average and lowest FPS get displayed and show up in different colors corrisponding to their values

--- Atomic Nucleus - Measuring Performance/Assets/Scripts/Nucleon.cs
+++ Atomic Nucleus - Measuring Performance/Assets/Scripts/Nucleon.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//If the rigidbody component is not found it gets created
 [RequireComponent(typeof(Rigidbody))]
 public class Nucleon : MonoBehaviour
 {
+    //References for the unity Inspector
     public float attractionForce;
 
-    Rigidbody body;
+    //Reference for the rigidbody
+    Rigidbody rb;
 
     private void Awake()
     {
-        body = GetComponent<Rigidbody>();
+        //Store the rigidbody component in rb to manipulate it
+        rb = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
-        body.AddForce(transform.localPosition * -attractionForce);
+        //Adding a force to the rigidbody that pushes him away from his current position
+        rb.AddForce(transform.localPosition * -attractionForce);
     }
 }
--- Atomic Nucleus - Measuring Performance/Assets/Scripts/NucleonSpawner.cs
+++ Atomic Nucleus - Measuring Performance/Assets/Scripts/NucleonSpawner.cs
@@ -4,26 +4,36 @@ using UnityEngine;
 
 public class NucleonSpawner : MonoBehaviour
 {
+    //Adjustable through inspector
     public float timeBetweenSpawns;
     public float spawnDistance;
+    //Array for the Prefabs in the inspector
     public Nucleon[] nucleonPrefabs;
 
+    //Keeping track of last spawn
     private float timeSinceLastSpawn;
 
     private void FixedUpdate()
     {
-        timeSinceLastSpawn += Time.deltaTime;
+        //Add fixedDeltaTime to time since last spawn
+        timeSinceLastSpawn += Time.fixedDeltaTime;
+        //If the time since last spawn is bigger then the time between spawns
         if (timeSinceLastSpawn >= timeBetweenSpawns)
         {
-            timeSinceLastSpawn = 0f;
+            //Spawn a nucleon
             SpawnNucleon();
+            //and reset the time since last spawn back to zero
+            timeSinceLastSpawn = 0f;
         }
     }
 
     void SpawnNucleon()
     {
+        //Getting a random nucleon from our prefab array
         Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
-        Nucleon spawn = Instantiate<Nucleon>(prefab);
-        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+        //Instantiting a Nucleon with the selected prefab
+        Nucleon instance = Instantiate<Nucleon>(prefab);
+        //and setting the local position of the nucleon to a random spherepoint times spawndistance
+        instance.transform.localPosition = Random.onUnitSphere * spawnDistance;
     }
 }
