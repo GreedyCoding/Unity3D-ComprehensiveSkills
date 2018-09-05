0d86b9728102dd89fc2496ee8d3714468b166188
GreedyCoding <marco_hofer2012@outlook.com>
2018-09-05 23:42:43 +0200

    Started working on ShapeFactory scriptable object

---
+++ Object Management/Assets/Scripts/ShapeFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ShapeFactory : ScriptableObject
+{
+    [SerializeField] private Shape[] prefabs;
+
+    public Shape GetShape (int shapeId)
+    {
+        return Instantiate(prefabs[shapeId]);
+    }
+
+    public Shape GetRandomShape()
+    {
+        return GetShape(Random.Range(0, prefabs.Length));
+    }
+}
