1789a709ed70cd2a8d44f11d41752f657892f92a
GreedyCoding <marco_hofer2012@outlook.com>
2018-09-15 18:14:22 +0200

    Started Mesh Basics
    Created a plane with UV mapping and a cube. The cube does not work yet though

---
+++ Mesh Basics/Assets/Scripts/CubeVertices.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class CubeVertices : MonoBehaviour
+{
+
+    [SerializeField] int xSize;
+    [SerializeField] int ySize;
+    [SerializeField] int zSize;
+
+    private Mesh mesh;
+
+    private Vector3[] vertices;
+
+    private void Awake()
+    {
+        GenerateMesh();
+    }
+
+    private void GenerateMesh()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Cube";
+        StartCoroutine(CreateVertices());
+        StartCoroutine(CreateTriangles());
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+
+    IEnumerator CreateVertices()
+    {
+		int cornerVertices = 8;
+		int edgeVertices = (xSize + ySize + zSize - 3) * 4;
+		int faceVertices = (
+			(xSize - 1) * (ySize - 1) +
+			(xSize - 1) * (zSize - 1) +
+			(ySize - 1) * (zSize - 1)) * 2;
+		vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
+
+        int v = 0;
+
+        //Looping through the layers of the cube
+        for (int y = 0; y <= ySize; y++)
+        {
+            //Creating a loop around the cube at the current y value(layer) 
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[v++] = new Vector3(x, y, 0);
+                yield return new WaitForSeconds(0.05f);
+
+            }
+            for (int z = 1; z <= zSize; z++)
+            {
+                vertices[v++] = new Vector3(xSize, y, z);
+                yield return new WaitForSeconds(0.05f);
+
+            }
+            for (int x = xSize - 1; x >= 0; x--)
+            {
+                vertices[v++] = new Vector3(x, y, zSize);
+                yield return new WaitForSeconds(0.05f);
+
+            }
+            for (int z = zSize - 1; z > 0; z--)
+            {
+                vertices[v++] = new Vector3(0, y, z);
+                yield return new WaitForSeconds(0.05f);
+
+            }
+        }
+
+        //Filling the holes at the top and bottom of the cube
+        for (int z = 1; z < zSize; z++)
+        {
+            for (int x = 1; x < xSize; x++)
+            {
+                vertices[v++] = new Vector3(x, ySize, z);
+                yield return new WaitForSeconds(0.05f);
+
+            }
+        }
+        for (int z = 1; z < zSize; z++)
+        {
+            for (int x = 1; x < xSize; x++)
+            {
+                vertices[v++] = new Vector3(x, 0, z);
+                yield return new WaitForSeconds(0.05f);
+            }
+        }
+    }
+
+    IEnumerator CreateTriangles()
+    {
+        //Calculating how many quads we have
+        int quads = ((xSize * ySize) + (xSize * zSize) + (ySize * zSize)) * 2;
+        //Creating int array with quads times 6 because there are 6 vertices needed for 1 quad
+        int[] triangles = new int[quads * 6];
+        //Calculating the size of the ring
+        int ring = (xSize + zSize) * 2;
+
+        int t = 0, v = 0;
+
+        for (int y = 0; y < ySize; y++, v++)
+        {
+            for (int q = 0; q < ring - 1; q++, v++)
+            {
+                t = SetQuad(triangles, t, v, v + 1, v + ring, v + ring + 1);
+            }
+            t = SetQuad(triangles, t, v, v - ring + 1, v + ring, v + 1);
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        mesh.triangles = triangles;
+    }
+
+    static int SetQuad(int[] triangles, int i, int v00, int v10, int v01, int v11)
+    {
+        triangles[i] = v00;
+        triangles[i + 1] = triangles[i + 4] = v01;
+        triangles[i + 2] = triangles[i + 3] = v10;
+        triangles[i + 5] = v11;
+        return i + 6;
+    }
+}
---
+++ Mesh Basics/Assets/Scripts/PlaneVertices.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class PlaneVertices : MonoBehaviour
+{
+    [SerializeField] int xSize;
+    [SerializeField] int ySize;
+
+    Vector3[] vertices;
+
+    Mesh mesh;
+
+    void Awake()
+    {
+        Generate();
+    }
+
+    void OnDrawGizmos()
+    {
+        //If there are no vertices we dont have to draw anything so we return
+        if(vertices == null)
+        {
+            return;
+        }
+
+        //Otherwise set Gizmo Color to Blact
+        Gizmos.color = Color.black;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            //And draw a sphere at all positions of the vertices
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+
+    void Generate()
+    {
+        //Getting the Meshfilter and setting its mesh to a new mesh
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        //Setting Name of the Mesh
+        mesh.name = "Procedural Face";
+
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+            }
+        }
+
+        //And an array with UV points for the UV map
+        Vector2[] uv = new Vector2[vertices.Length];
+        //Tangents for normal mapping
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+        //Loop through the columns
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            //And loop through all the points in this column
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                //And create a point with the x and y at this spot in the array
+                vertices[i] = new Vector3(x, y);
+                //And set a point for the UV map at the
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                //Setting tangent
+                tangents[i] = tangent;
+            }
+        }
+        //Setting the vertices, tangents and the uv points of the mesh
+        mesh.vertices = vertices;
+        mesh.tangents = tangents;
+        mesh.uv = uv;
+
+        //Int array with all the points the meshrenderer uses to draw triangles
+        int[] triangles = new int[xSize * ySize * 6]; //Multiplied by 6 because we need 6 points to draw 2 triangles (1 quad)
+        //Setting the points to draw for every triangle
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+        //Setting the triangles of the mesh to the calcualted points
+        mesh.triangles = triangles;
+
+        //And recalculate the normals of the mesh so lighting is rendered correctly
+        mesh.RecalculateNormals();
+
+    }
+}
