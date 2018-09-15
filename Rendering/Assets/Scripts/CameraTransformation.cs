b03a7c3543a032a9de9596a416ce8438a50df825
GreedyCoding <marco_hofer2012@outlook.com>
2018-09-15 14:32:53 +0200

    Finished First Scene in Rendering
    Added Dropdown to the CameraTransformation to select between  no/orthographic/perspective projection.

--- Rendering/Assets/Scripts/CameraTransformation.cs
+++ Rendering/Assets/Scripts/CameraTransformation.cs
@@ -1,19 +1,82 @@
 ﻿using UnityEngine;
 
+public enum ProjectionEnumerator
+{
+
+    NoProjection,
+    OrthographicProjection,
+    PerspectiveProjection
+
+}
+
 public class CameraTransformation : Transformation
 {
     [SerializeField] private float focalLength = 1f;
+
+    public ProjectionEnumerator projectionDropdown;
 
     public override Matrix4x4 Matrix
     {
         get
         {
-            Matrix4x4 matrix = new Matrix4x4();
-            matrix.SetRow(0, new Vector4(focalLength, 0f, 0f, 0f));
-            matrix.SetRow(1, new Vector4(0f, focalLength, 0f, 0f));
-            matrix.SetRow(2, new Vector4(0f, 0f, 1f, 0f));
-            matrix.SetRow(3, new Vector4(0f, 0f, 0f, 0f));
-            return matrix;
+            switch (projectionDropdown)
+            {
+                case ProjectionEnumerator.NoProjection:
+                {
+                    Matrix4x4 matrix = new Matrix4x4();
+                    matrix.SetRow(0, new Vector4(1f, 0f, 0f, 0f));
+                    matrix.SetRow(1, new Vector4(0f, 1f, 0f, 0f));
+                    matrix.SetRow(2, new Vector4(0f, 0f, 1f, 0f));
+                    matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+                    return matrix;
+                }
+                case ProjectionEnumerator.OrthographicProjection:
+                {
+                    Matrix4x4 matrix = new Matrix4x4();
+                    matrix.SetRow(0, new Vector4(focalLength, 0f, 0f, 0f));
+                    matrix.SetRow(1, new Vector4(0f, focalLength, 0f, 0f));
+                    matrix.SetRow(2, new Vector4(0f, 0f, 0f, 0f));
+                    matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+                    return matrix;
+                }
+                case ProjectionEnumerator.PerspectiveProjection:
+                {
+                    Matrix4x4 matrix = new Matrix4x4();
+                    matrix.SetRow(0, new Vector4(focalLength, 0f, 0f, 0f));
+                    matrix.SetRow(1, new Vector4(0f, focalLength, 0f, 0f));
+                    matrix.SetRow(2, new Vector4(0f, 0f, 0f, 0f));
+                    matrix.SetRow(3, new Vector4(0f, 0f, 1f, 0f));
+                    return matrix;
+                }
+                default:
+                {
+                    Matrix4x4 matrix = new Matrix4x4();
+                    matrix.SetRow(0, new Vector4(1f, 0f, 0f, 0f));
+                    matrix.SetRow(1, new Vector4(0f, 1f, 0f, 0f));
+                    matrix.SetRow(2, new Vector4(0f, 0f, 1f, 0f));
+                    matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+                    return matrix;
+                }
+
+            }
+            //if (isOrthographic)
+            //{
+            //    Matrix4x4 matrix = new Matrix4x4();
+            //    matrix.SetRow(0, new Vector4(focalLength, 0f, 0f, 0f));
+            //    matrix.SetRow(1, new Vector4(0f, focalLength, 0f, 0f));
+            //    matrix.SetRow(2, new Vector4(0f, 0f, 0f, 0f));
+            //    matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+            //    return matrix;
+            //}
+            //else 
+            //{
+            //    Matrix4x4 matrix = new Matrix4x4();
+            //    matrix.SetRow(0, new Vector4(1f, 0f, 0f, 0f));
+            //    matrix.SetRow(1, new Vector4(0f, 1f, 0f, 0f));
+            //    matrix.SetRow(2, new Vector4(0f, 0f, 1f, 0f));
+            //    matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+            //    return matrix;
+            //}
         }
     }
 }
